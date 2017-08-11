d778a91a88b8ec1261446bce990df6baec4051ce
zhangli <45641783@>
2017-08-11 17:56:05 +0800

    增加测试报告页面
    1、增加测试报告页面
    2、修改测试报告背景颜色

--- tyrant/webpack/components/SelectedMenu/index.js
+++ tyrant/webpack/components/SelectedMenu/index.js
@@ -7,7 +7,17 @@ import WriteWSTestcase from 'WriteWSTestcase';
 // import './index.css';
 
 var SelectedMenu = React.createClass({
+	iFrameHeight(event){
 
+        var iframe = event.target;
+        var contentWrapper = event.target.parentNode.parentNode.parentNode;
+        // var subWeb = document.frames ? document.frames["iframe"].document :
+        				// iframe.contentDocument;
+		// console.log(subWeb);
+	    if(iframe != null) {
+	        iframe.style.height = contentWrapper.offsetHeight + "px";
+	    }
+	},
     render() {
     	var selectedMenu = this.props.selectedMenu;
     	// console.log(ws);
@@ -33,9 +43,13 @@ var SelectedMenu = React.createClass({
 				    				setMainSidebarHeight={this.props.setMainSidebarHeight}
 				    				/>
 	                        :
-	                        	<div>
-	                        		test
-	                        	</div>
+	                        	selectedMenu.id===101
+	                        	?
+	                        		<iframe src="http://172.16.15.88:8080/font/tyrant/dist/wsReport.html" onLoad={this.iFrameHeight} style={{width:'100%',borderWidth:'0px'}} scrolling="no"/>
+	                        	:
+		                        	<div>
+		                        		test
+		                        	</div>
 				    	}
 		    		</section>
 				</div>;
