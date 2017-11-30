1c9cd5cc5cb9178a86b8880bd0a7070b50fcca94
snowINsummer <45641783@qq.com>
2017-11-30 14:56:54 +0800

    增加nginx后端口修改
    增加nginx后端口修改

--- tyrant/src/common/js/conf/server_conf.js
+++ tyrant/src/common/js/conf/server_conf.js
@@ -2,11 +2,7 @@
 (function() {
     if (window.TrantServerConf === undefined) {
         window.TrantServerConf = {
-            tyrant: "http://localhost:8080"
-        };
-    }
-    if (window.AliceServerConf === undefined) {
-        window.AliceServerConf = {
+            tyrant: "http://localhost:8080",
             alice: "http://localhost:8080"
         };
     }
--- tyrant/src/common/js/conf/xxd/server_conf.js
+++ tyrant/src/common/js/conf/xxd/server_conf.js
@@ -2,12 +2,8 @@
 (function() {
     if (window.TrantServerConf === undefined) {
         window.TrantServerConf = {
-            tyrant: "http://192.168.33.47:8080/tyrant"
-        };
-    }
-    if (window.AliceServerConf === undefined) {
-        window.AliceServerConf = {
-            alice: "http://192.168.33.47:8080/alice"
+            tyrant: "http://192.168.33.47/tyrant",
+            alice: "http://192.168.33.47/alice"
         };
     }
 })();
